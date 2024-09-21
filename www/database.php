5688c1c8effd59ce0a37339fd296f23a309a697d
Matheus-Pedro <matheus.caprioli.pedro@gmail.com>
2024-09-21 18:30:23 -0300

    CREATE API Task

--- www/database.php
+++ www/database.php
@@ -8,7 +8,6 @@ try {
     $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, 
     PDO::ERRMODE_EXCEPTION);
-echo '<h2>Conectado com sucesso.<h2>';
 } catch (PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
 }
